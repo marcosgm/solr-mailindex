e68745b950bc68f3e05686c52d25d77e073bbf34
Marcos Garcia <mgarcia@liferay-hydroqc-cluster1.mtllab.sfl>
2013-03-14 12:52:17 -0400

    refactoring. Now use Message-ID as UID

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Misc
+MAIL ARCHIVING AND INDEXING USING SOLR
 ====
 
-miscellaneous scripts
+This is a proof-of-concept of a tool used to download mails from an IMAP folder, parse them, store with the attachments, arhived in a filesystem and index the result using SOLR
+
+Mails can always be retrieved thanks to the archive filesystem structure. SOLR will index all fields and use a UUID field to match together the MAIL text and metadata with its attachments
--- mda.sh
+++ mda.sh
@@ -3,16 +3,40 @@
 #store mail in its full form under the MAILDIR folder
 #then call the archiveMail.sh method to separate the attachments and store them using a UUID folder in an archived file structure
 UUID=$(uuidgen)
-cat - > maildir/new/$UUID
-archived=$(./archiveMail.sh maildir/new/$UUID)
+MAILDIR="/opt/SOLRTEST/maildir"
+TMPMAIL=$MAILDIR/tmp/$UUID  #random temp folder
+cat - > $TMPMAIL
+MESSAGEHEADERS=$(head -n 200 $TMPMAIL) #need to to this in 2 steps to avoid: "Delivery error (command mda.sh 10064 wrote to stderr: head: write error: Broken pipe"
+MESSAGEID=$(echo $MESSAGEHEADERS |formail -xMessage-Id |sed 's/[< >]//g') #extract Message-ID, regardless of upper or lowercase
+#MESSAGEID=$UUID
 
+if [ -z $MESSAGEID ]; then
+    MESSAGEID=$UUID   
+fi
+FILEPATH=$MAILDIR/new/$MESSAGEID
+mv $TMPMAIL $FILEPATH
+#now we have in a single file $FILEPATH the full mail, MIME encoded
 
-
-#clean up empty files (MIME problem?"
 YEARMONTH=$(date +"%Y/%m")
-ARCHIVE="mailarchive"
-ARCHIVEDEST=$ARCHIVE/$YEARMONTH/$UUID
+ARCHIVE="/opt/SOLRTEST/mailarchive"
+MIMETOOL="ripmime -i - --overwrite -e -d " #receives MIME msg by stdin, overwrite if we process twice the same message (waranteed to be unique thanks to messageID folder
+ARCHIVEDEST=$ARCHIVE/$YEARMONTH/$MESSAGEID
+mkdir -p $ARCHIVEDEST
 
-for f in $(ls $ARCHIVEDEST/*); do test ! -s $f  && echo "$f is empty" && rm $f; done
+#lets call ripmime to chop the MIMEfile into multiple files with attachments
+cat $FILEPATH | $MIMETOOL $ARCHIVEDEST #will put files in -d $ARCHIVEDEST
+rm $FILEPATH #remove original MIME file
 
+#clean up empty files (ripmime problem?)
+for f in $(ls $ARCHIVEDEST/*); do 
+    test ! -s $f  && rm $f;  #if the file is empty, remove it
+done
+
+#now send the files to SOLR
+SOLRURL="http://liferay-hydroqc-cluster1.mtllab.sfl:8983/solr/mail/upload"
+ARCHIVEURL="http://archiveserver/$YEARMONTH/$MESSAGEID"
+
+#./endMIMEfolderToSOLR.py --folder $ARCHIVEDEST --solrurl $SOLRURL --archiveUrl $ARCHIVEURL
+
+#exit OK, probably getmail will remove the mail from IMAP
 exit 0
